ee4bab3e035cb60ee52bc6e374f2e95bbcbfde62
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2024-01-10 11:52:50 +0100

    calc1

--- archive-cenniki.php
+++ archive-cenniki.php
@@ -9,18 +9,27 @@
         <tr>
             
             <th scope="col">Produkt</th>
-            <th scope="col">Cena</th>
+            <th scope="col">Cena netto Agrosik</th>
+            <th scope="col">Cena + transport</th>
+            <th scope="col">Cena + 30%</th>
             <th scope="col"><Details></Details></th>
         
         </tr>
     </thead>
     <tbody>
         <?php while (have_posts()) : the_post(); ?>
-            
+            <?php $cenaAgrosik = get_field('cena_netto_agrosik');
+                  $cenaAgrosikItransport=$cenaAgrosik+2.3;
+                  $cenaZmarzaZl= $cenaAgrosikItransport+$cenaAgrosikItransport*0.3;
+                  $cenaZmarzaEuro = $cenaZmarzaZl/4.3;
+                  $cenaZmarzaEuroZaokraglona =round($cenaZmarzaEuro,2);
+            ?>
             <tr>
            
             <td><?php the_title() ?></td>
-            <td><?php the_field('cena') ?> euro</td>
+            <td><?php the_field('cena_netto_agrosik') ?> zł</td>
+            <td><?php echo $cenaAgrosikItransport ?> zł</td>
+            <td><?php echo $cenaZmarzaEuroZaokraglona?> euro</td>
             <td> <a href="<?php the_permalink(); ?>"> Zobacz</a></td>
            
         </tr>
@@ -30,4 +39,6 @@
     </div>
 <?php endif; ?>
 <?php endif; ?>
+<div>End of list</div>
 <?php get_footer(); ?>
+<?php //the_field('cena_+_transport_zl') ?> 
