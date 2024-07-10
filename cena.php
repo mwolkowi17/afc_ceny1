f1a309c04789a13fdf3547473e1a3ce0fe24009c
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2024-07-10 16:58:08 +0200

    new

--- cena.php
+++ cena.php
@@ -1,17 +1,19 @@
 <?php
 
-class CenaProduktu{
-    public $cena_agrosik;
-    public $cena_plus_transport;
-    public $cena_plus_transport_i_marza;
-    public $cena_puls_transport_i_marza_euro;
-    public $cena_puls_transport_i_marza_euro_zaokraglone;
+class CenaProduktu
+{
+  public $cena_agrosik;
+  public $cena_plus_transport;
+  public $cena_plus_transport_i_marza;
+  public $cena_puls_transport_i_marza_euro;
+  public $cena_puls_transport_i_marza_euro_zaokraglone;
 
-    public function __construct($wartosc) {
-        $this->cena_agrosik=$wartosc;
-        $this->cena_plus_transport=$this->cena_agrosik+2.3;
-        $this->cena_plus_transport_i_marza=$this->cena_plus_transport+$this->cena_plus_transport*0.3;
-        $this->cena_puls_transport_i_marza_euro=$this->cena_plus_transport_i_marza/4.3;
-        $this->cena_puls_transport_i_marza_euro_zaokraglone = round($this->cena_puls_transport_i_marza_euro,2);
-      }
+  public function __construct($wartosc)
+  {
+    $this->cena_agrosik = $wartosc;
+    $this->cena_plus_transport = $this->cena_agrosik + 2.3;
+    $this->cena_plus_transport_i_marza = $this->cena_plus_transport + $this->cena_plus_transport * 0.3;
+    $this->cena_puls_transport_i_marza_euro = $this->cena_plus_transport_i_marza / 4.3;
+    $this->cena_puls_transport_i_marza_euro_zaokraglone = round($this->cena_puls_transport_i_marza_euro, 2);
+  }
 }
